e8fff252e91c26158786c588340d2f295d404331
allseenn <allseen@live.ru>
2025-05-28 22:33:51 +0300

    + summary to web.py

--- web.py
+++ web.py
@@ -3,8 +3,7 @@ from fastapi import APIRouter
 
 routes = APIRouter(prefix="/api/web", tags=["Chapter 5. Pydantic, Type Hints, and Models Tour"])
 
-
-@routes.get("/creature")
+@routes.get("/creature", summary="5-11. Define a FastAPI web endpiint: web.py")
 def get_all() -> list[Creature]:
     from data import get_creatures
     return get_creatures()
