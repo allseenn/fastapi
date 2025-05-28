6e485b76f9e7b749620d897bef8991a6b4e56df1
allseenn <allseen@live.ru>
2025-05-28 22:28:14 +0300

    ~ web.py many errors

--- web.py
+++ web.py
@@ -1,10 +1,10 @@
 from model import Creature
 from fastapi import APIRouter
 
-router = APIRouter(prefix="/web), tags=["Chapter 5. Pydantic, Type Hints, and Models Tour"]
+routes = APIRouter(prefix="/web", tags=["Chapter 5. Pydantic, Type Hints, and Models Tour"])
 
 
-@router.get("/creature")
+@routes.get("/creature")
 def get_all() -> list[Creature]:
     from data import get_creatures
     return get_creatures()
