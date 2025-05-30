4868fb66bbbc73a39b621e13bef773f43ba40499
allseenn <allseen@live.ru>
2025-05-30 21:56:23 +0300

    - dep*.py +dependency.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from hello import routes as hello_routes
 from greet_async import routes as greet_routes
 from web import routes as web_routes
+from dependency import routes as dep_routes
 
 app = FastAPI(title="FastAPI",
 description="""
@@ -13,3 +14,4 @@ Bill Lubanovic
 app.include_router(hello_routes)
 app.include_router(greet_routes)
 app.include_router(web_routes)
+app.include_router(dep_routes)
