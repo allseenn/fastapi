1364392f2c3f2bd3d7d7cd98bedd18f6118bc0fa
allseen <allseen@gmail.com>
2025-05-30 09:15:07 +0000

    +dep.py

---
+++ dep.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI, Depends, Params
+
+app = FastAPI()
+
+def user_dep(name: str = Params, password: str = Params):
+    return {"name": name, "valid": True}
+
+@app.get("/user")
+
+def get_user(user: dict = Depends(user_dep)) -> dict:
+    return user
